5a20076d25d9ce2dcdc55b00edc0eeea492fc7c5
Chandler Juego <chandlerjuego101@gmail.com>
2025-06-13 15:49:40 -0700

    blocking = statement in procedural blocks

--- gpu_noob/modules/block_dispatch.v
+++ gpu_noob/modules/block_dispatch.v
@@ -16,8 +16,8 @@ module BlockDispatch #(
 
     // info for each compute unit
     input wire [NUM_CORES-1:0] core_done, // given by compute unit
-    output reg [NUM_CORES-1:0] core_start,
-    output reg [NUM_CORES-1:0] core_reset,
+    output reg [NUM_CORES-1:0] core_start, 
+    output reg [NUM_CORES-1:0] core_ready, // ready for a block
 
     // block_id assigned to each compute unit
     output reg [31:0] core_block_id [0:NUM_CORES-1],
@@ -36,17 +36,16 @@ assign num_blocks = (num_threads + block_dim - 1) / block_dim;
 integer i;
 always @ (posedge(clk)) begin
     if (rst) begin
-        core_reset <= 1;
-        core_start <= 0;
         blocks_dispatched <= 0;
         blocks_done <= 0;
         kernel_done <= 0;
 
-
         for (i = 0; i < NUM_CORES; i = i + 1) begin
             // TODO: they should be assigned a different default value since ZERO is a valid block id
             // reset assigned block ids of each core to a default value (zero)
-            core_block_id[i] <= 0;      
+            core_block_id[i] <= 0;
+            core_ready[i] <= 1;
+            core_start[i] <= 0;      
         end        
     end
 
@@ -57,25 +56,24 @@ always @ (posedge(clk)) begin
         end
 
         for (i = 0; i < NUM_CORES; i = i + 1) begin
-            if (core_reset[i]) begin
+            if (core_ready[i]) begin
                 // a core was recently reset (either finished executing a block or rst by gpu)
-                core_reset[i] <= 0;
+                core_ready[i] <= 0;
 
                 // check if there is a block that can be given
                 if (blocks_dispatched < num_blocks) begin
                     core_block_id[i] <= blocks_dispatched; // give a block to a core
-                    blocks_dispatched <= blocks_dispatched + 1;
-                    core_start[i] <= 1; 
+                    core_start[i] <= 1;
+                    blocks_dispatched = blocks_dispatched + 1; 
                 end
             end
 
             if (core_done[i]) begin
-                // check if a compute unit has finished its block and reset it
-                blocks_done <= blocks_done + 1;
-                core_reset[i] <= 1;
+                // check if a compute unit has finished its block and set it back to ready state
+                core_ready[i] <= 1;
                 core_start[i] <= 0;
+                blocks_done = blocks_done + 1;
             end
-
         end
     end
 end
