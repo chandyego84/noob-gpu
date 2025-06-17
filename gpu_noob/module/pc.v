5ce09f4c603dac37abb38f8ce27e1947283a99df
Chandler Juego <chandlerjuego101@gmail.com>
2025-06-17 12:17:34 -0700

    waves/simd set to 1

--- gpu_noob/module/pc.v
+++ gpu_noob/module/pc.v
@@ -4,7 +4,7 @@
 // No branching implemented
 module PC #(
     parameter PROGRAM_MEM_ADDR_WIDTH = 32, // program memory addresses are 32b, though actual address space is much smaller
-    parameter NUM_WAVES = 5 // max number of waves/SIMD
+    parameter WAVES_PER_SIMD = 5 // max number of waves/SIMD 
 )
 (
     input wire clk,
@@ -15,18 +15,18 @@ module PC #(
     input wire DISPATCH_NEW_WAVE, // signal to indicate a new wave was dispatched to SIMD unit
 
     // contexts
-    input wire [$clog2(NUM_WAVES)-1:0] active_context,
+    input wire [$clog2(WAVES_PER_SIMD)-1:0] active_context,
 
     // outputs
     output reg [PROGRAM_MEM_ADDR_WIDTH-1:0] pc_out // PC of the active wavefront
 );
 
-reg [PROGRAM_MEM_ADDR_WIDTH-1:0] pc_contexts [0:NUM_WAVES-1]; // track PC context for each wave
+reg [PROGRAM_MEM_ADDR_WIDTH-1:0] pc_contexts [0:WAVES_PER_SIMD-1]; // track PC context for each wave
 
 always @ (posedge(clk)) begin
     if (rst) begin
         integer i;
-        for (i = 0; i < NUM_WAVES; i++) begin
+        for (i = 0; i < WAVES_PER_SIMD; i++) begin
             // reset each context pc value to 0
             pc_contexts[i] <= 0;
         end        
